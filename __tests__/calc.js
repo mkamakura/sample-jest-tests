e9b476262dffbc23d087d79c5de6200661dfcbb7
mkamakura <kamakura.masaya@gmail.com>
2018-01-21 01:11:53 +0900

    test(calc): add calc tests

---
+++ __tests__/calc.js
@@ -0,0 +1,9 @@
+import assert from 'assert'
+import rewire from 'rewire'
+
+const calc = rewire('../src/calc.js')
+const sum = calc.__get__('sum')
+
+test('calc:sum should return sum result', () => {
+  assert.equal(sum(1,2), 3)
+})
