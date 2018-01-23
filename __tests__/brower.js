6bbdd98e48c714e0232610bc31a3f39fba6803f4
mkamakura <kamakura.masaya@gmail.com>
2018-01-23 23:46:40 +0900

    chore(prettier): add prettier and add fmt task

--- __tests__/brower.js
+++ __tests__/brower.js
@@ -1,25 +1,29 @@
-import assert from 'assert'
-import { get_user_agent, get_search } from '../src/browser'
+import assert from "assert"
+import { get_user_agent, get_search } from "../src/browser"
 
-Object.defineProperty(window.navigator, "userAgent", ((_value) => {
-  return {
-    get: function _get() {
-      return _value;
-    },
-    set: function _set(v) {
-      _value = v;
+Object.defineProperty(
+  window.navigator,
+  "userAgent",
+  (_value => {
+    return {
+      get: function _get() {
+        return _value
+      },
+      set: function _set(v) {
+        _value = v
+      }
     }
-  }
-})(window.navigator.userAgent))
+  })(window.navigator.userAgent)
+)
 
-test('browser: get user agent', () => {
-  window.navigator.userAgent = 'Chrome'
-  assert.equal(get_user_agent(), 'Chrome')
+test("browser: get user agent", () => {
+  window.navigator.userAgent = "Chrome"
+  assert.equal(get_user_agent(), "Chrome")
 })
 
-test('browser: get search', () => {
+test("browser: get search", () => {
   jsdom.reconfigure({
     url: "https://www.example.com?date=20180101"
   })
-  assert.deepEqual(get_search(), { date: '20180101' })
+  assert.deepEqual(get_search(), { date: "20180101" })
 })
--- src/browser.js
+++ src/browser.js
@@ -5,7 +5,7 @@ export function get_user_agent() {
 export function get_search() {
   return window.location.search
     .slice(1)
-    .split('&')
-    .map(p => p.split('='))
+    .split("&")
+    .map(p => p.split("="))
     .reduce((obj, [k, v]) => ({ ...obj, [k]: v }), {})
 }
